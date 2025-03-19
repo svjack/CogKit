f3b8576ada162e9a15442347d940f292ba2f621b
Chenhui Zhang <zhang.chenhui@outlook.com>
2025-03-19 20:39:22 +0800

    chore: updates pyproject metadata

--- src/cogkit/api/routers/images.py
+++ src/cogkit/api/routers/images.py
@@ -14,18 +14,20 @@ from cogkit.api.services import ImageGenerationService
 
 router = APIRouter()
 
+
 def np_to_base64(image_array: np.ndarray) -> str:
     byte_stream = image_array.tobytes()
-    base64_str = base64.b64encode(byte_stream).decode('utf-8')
+    base64_str = base64.b64encode(byte_stream).decode("utf-8")
     return base64_str
 
-@router.post("/generations")
+
+@router.post("/generations", response_model=ImagesResponse)
 def generations(
     image_generation: Annotated[ImageGenerationService, Depends(get_image_generation_service)],
     params: ImageGenerationParams,
 ) -> ImagesResponse:
-    images_list = image_generation.generate(
+    images_lst = image_generation.generate(
         model=params.model, prompt=params.prompt, size=params.size, num_images=params.n
     )
-    images_base64 = [ImageInResponse(b64_json=np_to_base64(image)) for image in images_list]
+    images_base64 = [ImageInResponse(b64_json=np_to_base64(image)) for image in images_lst]
     return ImagesResponse(created=int(time.time()), data=images_base64)
