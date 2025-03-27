4ad31dcb7b678c96e4a7abdb8e661f182fa6da82
Yuxuan Zhang <2448370773@qq.com>
2025-03-27 16:53:58 +0800

    support new resolutions

--- src/cogkit/api/models/images/generation_params.py
+++ src/cogkit/api/models/images/generation_params.py
@@ -11,7 +11,14 @@ class ImageGenerationParams(RequestParams):
     model: str = "cogview-4"
     n: int = 1
     size: Literal[
-        "1024x1024", "768x1344", "864x1152", "1344x768", "1152x864", "1440x720", "720x1440"
+        "1024x1024",
+        "768x1024",
+        "768x1344",
+        "864x1152",
+        "1344x768",
+        "1152x864",
+        "1440x720",
+        "720x1440",
     ] = "1024x1024"
     num_inference_steps: int = 50
     guidance_scale: float = 3.5
