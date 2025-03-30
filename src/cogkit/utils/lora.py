6fde85f0da04a166bfbfbae741d2a422e9d4d272
Yuxuan Zhang <2448370773@qq.com>
2025-03-30 12:59:08 +0800

    use with load lora weight

--- src/cogkit/utils/lora.py
+++ src/cogkit/utils/lora.py
@@ -9,8 +9,8 @@ def load_lora_checkpoint(
     lora_model_id_or_path: str,
     lora_scale: float = 1.0,
 ) -> None:
-    pipeline.load_lora_weights(lora_model_id_or_path)
-    pipeline.fuse_lora(components=["transformer"], lora_scale=lora_scale)
+    pipeline.load_lora_weights(lora_model_id_or_path, lora_scale=lora_scale)
+    # pipeline.fuse_lora(components=["transformer"], lora_scale=lora_scale)
 
 
 def unload_lora_checkpoint(
